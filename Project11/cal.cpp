9756881193da6414a0ab7242d887871f27decc15
hyunsung.jin <darkfishno1@gmail.com>
2023-06-01 14:07:35 +0900

    구현완료

--- Project11/cal.cpp
+++ Project11/cal.cpp
@@ -3,6 +3,28 @@ class Account
 public:
 	explicit Account(int balance) : balance(balance) {
 	}
+
+	void setFiveInterestRatio()
+	{
+		interestRatio = 1.05f;
+		balance = balance * interestRatio;
+	}
+
+	void setInterestRatio(float ratio)
+	{
+		interestRatio = 1.0f + ratio;
+		balance = balance * interestRatio;
+	}
+
+	int getAfterNyearBalance(int NYear)
+	{
+		for(int n = 0; n < NYear; n++)
+		{
+			balance = balance * interestRatio;
+		}
+		return balance;
+	}
+
 	int getBalance(){
 		return balance;
 	}
@@ -16,6 +38,7 @@ public:
 	}
 private:
 	int balance = 10000;
+	float interestRatio = 1.0f;
 };
 
 
