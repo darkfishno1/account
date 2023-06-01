8f0c4abfe24de068cf8f512ce4d8397cc35fc5ff
hyunsung.jin <darkfishno1@gmail.com>
2023-06-01 13:48:36 +0900

    init commit

---
+++ Sample-Test1/test.cpp
@@ -0,0 +1,24 @@
+#include "pch.h"
+#include <vector>
+#include "../Project11/cal.cpp"
+
+using namespace std;
+
+class AccountFixture : public testing::Test
+{
+public:
+	Account account{10000};
+};
+TEST_F(AccountFixture, CreateAcountInit10000won) {
+	EXPECT_EQ(10000, account.getBalance());
+}
+
+TEST_F(AccountFixture, Deposit) {
+	account.deposit(500);
+	EXPECT_EQ(10500, account.getBalance());
+}
+
+TEST_F(AccountFixture, Withdraw) {
+	account.withdraw(600);
+	EXPECT_EQ(9400, account.getBalance());
+}
